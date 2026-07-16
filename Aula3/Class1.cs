9cfef34e03c42368253b0b24511b10985c3dc36b
joaotech12 <zeroo01010010@gmail.com>
2026-07-16 20:15:53 -0300

    ok

---
+++ Aula3/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Aula3
+{
+    internal class Class1
+    {        
+       public static void Execute()
+        {
+          
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            celsius = (celsius * 9 / 5) + 32;
+            double fahrenheit = celsius;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	   int farenheit = 32;
+        double celsius = (farenheit - 32) * 5.0 / 9.0;
+        Console.WriteLine(celsius);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
