6d08d30c27fa76ee18c1a6071fc282bebac289e2
joaotech12 <zeroo01010010@gmail.com>
2026-07-15 21:08:14 -0300

    ok

--- Aula2/Class1.cs
+++ Aula2/Class1.cs
@@ -6,5 +6,59 @@ namespace ConsoleApp1.Aula2
 {
     internal class Class1
     {
-    }
-}
+        public static void Main()
+        {
+           
+
+            int numero1 = 10;
+            int numero2 = 5;
+            int somatotal = numero1 + numero2;
+            Console.WriteLine(somatotal);
+
+
+            int numero3 = 10;
+            int numero4 = 5;
+            Console.WriteLine(numero3 - numero4);
+
+            int numero5 = 10;
+            int numero6 = 5;
+            Console.WriteLine(numero5 * numero6);
+
+            int numero7 = 10;
+            int numero8 = 5;
+            Console.WriteLine(numero7 / numero8);
+
+
+        }
+    }  
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
